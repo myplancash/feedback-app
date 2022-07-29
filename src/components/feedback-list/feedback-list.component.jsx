1e9a371615ff3790ab6c6b530859e89fb9079e00
sergio-estebitan <myplancash@gmail.com>
2022-07-29 16:25:41 -0500

    initial commit 2

--- src/components/feedback-list/feedback-list.component.jsx
+++ src/components/feedback-list/feedback-list.component.jsx
@@ -15,25 +15,10 @@ const FeedbackList = ({ feedback, handleDelete }) => {
 
   return (
     <FeedbackListDiv>
-      <AnimatePresence>
-        {feedback &&
-          feedback.map((item) => (
-            <motion.div
-              style={{ width: '100%' }}
-              key={item.id}
-              initial={{ x: 0, opacity: 0 }}
-              animate={{ x: [null, 100, 0], opacity: 1 }}
-              transition={{ ease: 'easeOut', duration: 0.5 }}
-              exit={{ x: 0, opacity: 0 }}
-            >
-              <FeedbackItem
-                key={item.id}
-                item={item}
-                handleDelete={handleDelete}
-              />
-            </motion.div>
-          ))}
-      </AnimatePresence>
+      {feedback &&
+        feedback.map((item) => (
+          <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+        ))}
     </FeedbackListDiv>
   )
 }
